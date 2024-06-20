485848566e6c24020e56d150ff5792a6209ac17a
mssgatsby <mssgatsby@gmail.com>
2024-06-20 14:49:15 +0500

    Solved remove dublicate problem

--- script.js
+++ script.js
@@ -145,3 +145,21 @@ var isValid = function (s) {
   }
   return stack.length === 0;
 };
+
+////////////////////////////////////////
+// Remove Duplicates from Sorted Array
+
+var removeDuplicates = function (nums) {
+  if (nums.length === 0) return 0;
+
+  let k = 1;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[k + 1] !== nums[i]) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+  console.log(k);
+};
+
+removeDuplicates([1, 2, 3, 3, 3, 4]);
