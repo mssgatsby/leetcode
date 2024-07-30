9492be06e0f45da8e2fa2effad048e64286b3fa2
mssgatsby <mssgatsby@gmail.com>
2024-07-30 17:19:26 +0500

    Problem 88 solved

--- script.js
+++ script.js
@@ -388,6 +388,38 @@ var deleteDuplicates = function (head) {
   return head;
 };
 
+//////////////////////////////////
+// 88. Merge Sorted Array
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function (nums1, m, nums2, n) {
+  let i = m - 1;
+  let j = n - 1;
+  let k = m + n - 1;
+
+  while (i >= 0 && j >= 0) {
+    if (nums1[i] > nums2[j]) {
+      nums1[k] = nums1[i];
+      i -= 1;
+    } else {
+      nums1[k] = nums2[j];
+      j -= 1;
+    }
+    k--;
+  }
+  while (j >= 0) {
+    nums1[k] = nums2[j];
+    j -= 1;
+    k--;
+  }
+};
+
 //////////////////////////////
 // 2235.Add Two Integers
 
